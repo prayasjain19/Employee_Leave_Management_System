56757398af2a00042b1a0c18337679cac2850bfe
prayasjain8765@gmail.com <prayasjain8765@gmail.com>
2025-08-21 15:28:48 +0530

    Employee Leave

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: process.env.CLIENT_URL, // frontend URL
+  "*", // frontend URL
   credentials: true
 }));
 app.use(cookieParser());
