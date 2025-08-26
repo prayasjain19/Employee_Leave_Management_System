c81a2a7c4621a3d1be358894699d03cf5e0e49c4
prayasjain8765@gmail.com <prayasjain8765@gmail.com>
2025-08-26 16:44:58 +0530

    "Email Updated"

--- backend/services/email.service.js
+++ backend/services/email.service.js
@@ -1,7 +1,7 @@
 const nodemailer = require('nodemailer');
 const dotenv = require('dotenv');
 dotenv.config();
-
+//Email Service
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
